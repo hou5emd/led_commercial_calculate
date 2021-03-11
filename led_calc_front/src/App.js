4082335fd20b483b4425396df6ee6a22eb68139a
hou5emd <andrandrey@live.ru>
2021-03-12 00:24:10 +0500

    Start...

--- led_calc_front/src/App.js
+++ led_calc_front/src/App.js
@@ -1,12 +1,47 @@
 import logo from './logo.svg';
+import VARS from'./VARS.js';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      HI
-    </div>
-  );
+    return (
+      <CalcForm />
+    );
+}
+
+class CalcForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            typeofLedScreen: 'videoEkranyUlichnyes'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        console.log('Вы выбрали' + this.state.value);
+        event.preventDefault();
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Выберите считаемую продукцию:
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="videoEkranyUlichnyes">Уличные видеоэкраны</option>
+                        <option value="videoEkranyInterernye">Интерьерные видеоэкраны</option>
+                        <option value="mediafasadies">Медиафасады</option>
+                    </select>
+                </label>
+                <input type="submit" value="Далее" />
+            </form>
+        );
+    }
+
 }
 
 export default App;
