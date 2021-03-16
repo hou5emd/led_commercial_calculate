1b3f7a4631b7f829d45fd879bca723d029303554
hou5emd <andrandrey@live.ru>
2021-03-16 17:51:32 +0500

    456789

--- led_calc_front/src/VARS.js
+++ led_calc_front/src/VARS.js
@@ -1,13 +1,30 @@
 import ApolloClient from 'apollo-boost';
+import { parseJSON } from './comp/resptests';
 import SelTapeProd from "./comp/tapeproduktion";
 
 
+async function usdrub(){
+    try{
+        let requestList = await fetch("https://www.freeforexapi.com/api/live?pairs=USDRUB",{
+             
+            mode: 'no-cors',
+           
 
+
+        });
+        console.log(requestList);
+        return requestList.json();
+    } catch {
+
+    }
+    
+}
 
 
 const VARS = {
     URL: "http://192.168.0.42:1337/",
     client: '',
+    usdrub: usdrub(),
     typeOfProductID: '',
     module: {},
     cabinet:{},
